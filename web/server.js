93b06a93ef008014739fc08d16bf22e20dab4597
Jam Risser <jam@jamrizzi.com>
2018-04-26 21:35:16 +0200

    Put server code in try catch

--- web/server.js
+++ web/server.js
@@ -12,11 +12,11 @@ const app = express();
 app.disable('x-powered-by');
 app.use(express.static(config.paths.distPublic));
 app.get('/*', async (req, res, next) => {
-  AppRegistry.registerComponent('App', () => App);
-  const { element, getStyleElement } = AppRegistry.getApplication('App', {});
-  const appHtml = renderToString(element);
-  const appCss = renderToStaticMarkup(getStyleElement());
   try {
+    AppRegistry.registerComponent('App', () => App);
+    const { element, getStyleElement } = AppRegistry.getApplication('App', {});
+    const appHtml = renderToString(element);
+    const appCss = renderToStaticMarkup(getStyleElement());
     const $ = cheerio.load(indexHtml);
     $('title').text(config.title);
     $('head').append(appCss);
