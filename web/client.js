3a6d94a38a29ca335c6295c58f78de613aec87fb
Jam Risser <jam@jamrizzi.com>
2018-05-05 16:08:34 +0200

    Updated hot reloading

--- web/client.js
+++ web/client.js
@@ -2,17 +2,28 @@ import 'babel-polyfill';
 import App from '~/App';
 import ignoreWarnings from 'ignore-warnings';
 import { AppRegistry } from 'react-native';
+import log from 'reaction/log';
+import React from 'react';
+import ReactDOM from 'react-dom';
 
 ignoreWarnings([
   'Calling ReactDOM.render() to hydrate server-rendered markup will stop working in React v17'
 ]);
 
 AppRegistry.registerComponent('App', () => App);
+renderApp();
 
-AppRegistry.runApplication('App', {
-  initialProps: {},
-  // eslint-disable-next-line no-undef
-  rootTag: document.getElementById('app')
-});
+if (module.hot) {
+  module.hot.accept('~/App', () => {
+    AppRegistry.registerComponent('App', () => require('~/App').default);
+    renderApp();
+  });
+}
 
-if (module.hot) module.hot.accept();
+function renderApp() {
+  AppRegistry.runApplication('App', {
+    initialProps: {},
+    // eslint-disable-next-line no-undef
+    rootTag: document.getElementById('app')
+  });
+}
