825f63ab01a23de5c99a0063bb24d7d13bc5bc00
Jam Risser <jam@codejam.ninja>
2018-06-21 10:43:11 +0530

    Added expo init script

--- src/expo.js
+++ src/expo.js
@@ -1,6 +1,6 @@
 // eslint-disable-next-line import/no-unresolved
 import { KeepAwake, registerRootComponent } from 'expo';
-import App from '~/App';
+import App from '~/../expo';
 
 if (process.env.NODE_ENV === 'development') KeepAwake.activate();
 
