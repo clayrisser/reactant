f924321f9182dfa6f8122a81b1e0bdc4c25c7aeb
Jam Risser <jam@codejam.ninja>
2018-07-02 12:24:07 +0530

    Updated reaction-build

--- storybook/stories/index.js
+++ storybook/stories/index.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import { Text } from 'react-native';
 
-import { storiesOf } from '@storybook/react-native';
+import storiesOf from 'reaction/storiesOf';
 import { action } from '@storybook/addon-actions';
 import { linkTo } from '@storybook/addon-links';
 
@@ -9,7 +9,9 @@ import Button from './Button';
 import CenterView from './CenterView';
 import Welcome from './Welcome';
 
-storiesOf('Welcome', module).add('to Storybook', () => <Welcome showApp={linkTo('Button')} />);
+storiesOf('Welcome', module).add('to Storybook', () => (
+  <Welcome showApp={linkTo('Button')} />
+));
 
 storiesOf('Button', module)
   .addDecorator(getStory => <CenterView>{getStory()}</CenterView>)
