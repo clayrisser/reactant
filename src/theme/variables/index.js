c141e9347a8222c102e159b804b164ee919e62ea
Jam Risser <jam@codejam.ninja>
2018-07-02 15:58:28 +0530

    Finished native base varaibles

---
+++ src/theme/variables/index.js
@@ -0,0 +1,6 @@
+import commonColor from './commonColor';
+import material from './material';
+import platform from './platform';
+
+export { commonColor, material, platform };
+export default { commonColor, material, platform };
--- web/ClientApp.js
+++ web/ClientApp.js
@@ -3,13 +3,13 @@ import { BrowserRouter } from 'react-router-dom';
 import { StyleProvider, Root } from 'native-base';
 import App from '../src/App';
 import getTheme from '../src/theme/components';
-import variables from '../src/theme/variables';
+import { material } from '../src/theme/variables';
 
 export default class ClientApp extends Component {
   render() {
     return (
       <BrowserRouter {...this.props}>
-        <StyleProvider style={getTheme(variables)}>
+        <StyleProvider style={getTheme(material)}>
           <Root>
             <App />
           </Root>
