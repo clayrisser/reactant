5b29cebeb327b0f4e08df5386135dc00febd820a
Jam Risser <jam@codejam.ninja>
2018-07-03 16:53:23 +0530

    Initial working version

--- src/index.js
+++ src/index.js
@@ -1,10 +1,12 @@
+import assets from 'reaction-base/lib/assets';
 import config from 'reaction-base/lib/config';
 import Link from './Link';
 import log from './log';
 
-export { Link, config, log };
+export { Link, assets, config, log };
 export default {
   Link,
+  assets,
   config,
   log
 };
--- src/log.js
+++ src/log.js
@@ -1,2 +1,51 @@
-// eslint-disable-next-line no-console
-export default console.log;
+import _ from 'lodash';
+import { Platform } from 'react-native';
+
+const log = createLogger();
+
+function createLogger() {
+  if (Platform.OS === 'web') {
+    // eslint-disable-next-line global-require
+    const { getRuntime } = require('cross-environment');
+    // eslint-disable-next-line global-require
+    const loglevel = require('loglevel');
+    if (getRuntime() === 'browser') {
+      loglevel.setLevel(
+        // eslint-disable-next-line no-undef
+        _.get(window, 'reaction.config.environment') === 'production'
+          ? 'error'
+          : 'info'
+      );
+      return loglevel;
+    }
+    // eslint-disable-next-line global-require
+    const { Logger, transports } = require('winston');
+    return new Logger({
+      level: 'info',
+      exitOnError: false,
+      transports: [
+        new transports.Console({
+          prettyPrint: true,
+          colorize: true,
+          showLevel: false
+        })
+      ]
+    });
+  }
+  // eslint-disable-next-line no-console
+  return console.log;
+}
+
+export function setLevel(level) {
+  if (Platform.OS === 'web') {
+    // eslint-disable-next-line global-require
+    const { getRuntime } = require('cross-environment');
+    if (getRuntime() === 'browser') {
+      log.setLevel(level);
+    } else {
+      log.level = level;
+    }
+  }
+}
+
+export default log;
