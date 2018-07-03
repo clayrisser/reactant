d5bf8e6748e8369979c81f45c593e576a45be181
Jam Risser <jam@codejam.ninja>
2018-07-03 12:28:23 +0530

    Initial commit

---
+++ src/Link/index.android.js
@@ -0,0 +1,3 @@
+import { Link } from 'react-router-native';
+
+export default Link;
---
+++ src/index.js
@@ -0,0 +1,10 @@
+import config from 'reaction-base/lib/config';
+import Link from './Link';
+import log from './log';
+
+export { Link, config, log };
+export default {
+  Link,
+  config,
+  log
+};
