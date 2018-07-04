6403db08df5d2bfe839fbac2de28374cd7641257
Jam Risser <jam@codejam.ninja>
2018-07-04 07:01:15 +0530

    Web and native compatible

--- src/routes/Splash.js
+++ src/routes/Splash.js
@@ -1,16 +1,21 @@
+import PropTypes from 'prop-types';
 import React, { Component } from 'react';
-import { Text, Content } from 'native-base';
 import { Link } from 'reaction-base';
+import { Text, View } from 'native-base';
 
 export default class Splash extends Component {
+  static propTypes = {
+    history: PropTypes.object.isRequired
+  };
+
   render() {
     return (
-      <Content>
+      <View>
         <Text>Loading . . .</Text>
         <Link to="/todo-list">
           <Text>ToDo List</Text>
         </Link>
-      </Content>
+      </View>
     );
   }
 
--- src/routes/TodoList.js
+++ src/routes/TodoList.js
@@ -1,17 +1,15 @@
 import React, { Component } from 'react';
-import { List, Content } from 'native-base';
+import { List } from 'native-base';
 import TodoItem from '~/components/TodoItem';
 
 export default class TodoList extends Component {
   render() {
     return (
-      <Content>
-        <List>
-          <TodoItem>Clean the Kitchen</TodoItem>
-          <TodoItem finished>Make the Bed</TodoItem>
-          <TodoItem>Eat Breakfast</TodoItem>
-        </List>
-      </Content>
+      <List>
+        <TodoItem>Clean the Kitchen</TodoItem>
+        <TodoItem finished>Make the Bed</TodoItem>
+        <TodoItem>Eat Breakfast</TodoItem>
+      </List>
     );
   }
 }
