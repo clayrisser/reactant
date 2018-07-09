cf12311b26344953981aa6c2eb44dd79c4f46c55
Jam Risser <jam@codejam.ninja>
2018-07-10 02:11:19 +0530

    Revert "Simplified logger"
    This reverts commit 42764b3606856cf1694c8750b7070a37a8cb3b21.

---
+++ src/log/index.js
@@ -0,0 +1,42 @@
+import _ from 'lodash';
+import loglevel from 'loglevel';
+import { Logger, transports } from 'winston';
+
+// eslint-disable-next-line no-new-func
+const isBrowser = new Function(
+  'try { return this === window } catch (e) { return false }'
+);
+const log = createLogger();
+
+function createLogger() {
+  if (isBrowser()) {
+    loglevel.setLevel(
+      // eslint-disable-next-line no-undef
+      _.get(window, 'reaction.config.environment') === 'production'
+        ? 'error'
+        : 'info'
+    );
+    return loglevel;
+  }
+  return new Logger({
+    level: 'info',
+    exitOnError: false,
+    transports: [
+      new transports.Console({
+        prettyPrint: true,
+        colorize: true,
+        showLevel: false
+      })
+    ]
+  });
+}
+
+export function setLevel(level) {
+  if (isBrowser()) {
+    log.setLevel(level);
+  } else {
+    log.level = level;
+  }
+}
+
+export default log;
