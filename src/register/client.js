3724c2a4fa68932935442d5391f146117c45fbfd
Jam Risser <jam@codejam.ninja>
2018-07-12 03:08:52 +0530

    Working with registers

--- src/register/client.js
+++ src/register/client.js
@@ -20,7 +20,7 @@ async function renderClient(initialProps) {
     });
   });
   initialProps.context = context;
-  const ClientApp = require('../../../../web/ClientApp').default;
+  const ClientApp = require('../../../web/ClientApp').default;
   AppRegistry.registerComponent('App', () => ClientApp);
   AppRegistry.runApplication('App', {
     initialProps,
@@ -37,7 +37,7 @@ export default function client(initialProps = {}) {
   if (config.options.debug) setLevel('debug');
   if (module.hot)
     module.hot.accept(
-      '../../../../web/ClientApp',
+      '../../../web/ClientApp',
       renderClient.bind(this, initialProps)
     );
   renderClient(initialProps).catch(console.error);
