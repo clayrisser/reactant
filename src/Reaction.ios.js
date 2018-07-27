7b1a0f5b903a37e2ef750cec6a8b7e603af7d86c
Jam Risser <jam@codejam.ninja>
2018-07-27 14:27:09 +0200

    Fixed web bug from storybook

--- src/Reaction.ios.js
+++ src/Reaction.ios.js
@@ -3,14 +3,23 @@ import React, { Component } from 'react';
 import { ConnectedRouter } from 'connected-react-router';
 import { PersistGate } from 'redux-persist/integration/react';
 import { Provider } from 'react-redux';
-import App from '../../src/App';
+import { config } from 'reaction-base';
 
 export default class Reaction extends Component {
   static propTypes = {
     context: PropTypes.object.isRequired
   };
 
+  constructor(props) {
+    super(props);
+    this.App = require('../../src/App').default;
+    if (config.action === 'storybook') {
+      this.App = require('../../storybook').default;
+    }
+  }
+
   render() {
+    const { App } = this;
     const { store, persistor, history } = this.props.context;
     return (
       <Provider store={store}>
