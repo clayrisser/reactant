80a236a8c67d6a75d7a801b5399e163f1476d958
Jam Risser <jam@codejam.ninja>
2018-07-29 21:53:33 +0530

    Pass context to children

--- src/Reaction.android.js
+++ src/Reaction.android.js
@@ -1,34 +1,59 @@
 import PropTypes from 'prop-types';
 import React, { Component } from 'react';
+import _ from 'lodash';
 import { ConnectedRouter } from 'connected-react-router';
 import { PersistGate } from 'redux-persist/integration/react';
 import { Provider } from 'react-redux';
 import { config } from 'reaction-base';
 
-export default class Reaction extends Component {
-  static propTypes = {
-    context: PropTypes.object.isRequired
-  };
+function ReactionWrapper(props) {
+  class Reaction extends Component {
+    static propTypes = {
+      context: PropTypes.object.isRequired
+    };
 
-  constructor(props) {
-    super(props);
-    this.App = require('../../src/App').default;
-    if (config.options.storybook) {
-      this.App = require('../../storybook').default;
+    constructor(props) {
+      super(props);
+      this.App = require('../../src/App').default;
+      if (config.options.storybook) {
+        this.App = require('../../storybook').default;
+      }
+    }
+
+    getChildContext() {
+      return this.props.context;
+    }
+
+    render() {
+      const { App } = this;
+      const { store, persistor, history } = this.props.context;
+      return (
+        <Provider store={store}>
+          <PersistGate loading={null} persistor={persistor}>
+            <ConnectedRouter history={history}>
+              <App {...this.props} />
+            </ConnectedRouter>
+          </PersistGate>
+        </Provider>
+      );
     }
   }
-
-  render() {
-    const { App } = this;
-    const { store, persistor, history } = this.props.context;
-    return (
-      <Provider store={store}>
-        <PersistGate loading={null} persistor={persistor}>
-          <ConnectedRouter history={history}>
-            <App {...this.props} />
-          </ConnectedRouter>
-        </PersistGate>
-      </Provider>
-    );
-  }
+  const childContextTypes = {};
+  _.each(props.context, (item, key) => {
+    if (_.isArray(item)) {
+      return (childContextTypes[key] = PropTypes.array.isRequired);
+    }
+    if (_.isFunction(item)) {
+      return (childContextTypes[key] = PropTypes.func.isRequired);
+    }
+    return (childContextTypes[key] = PropTypes[typeof item].isRequired);
+  });
+  Reaction.childContextTypes = childContextTypes;
+  return <Reaction {...props} />;
 }
+
+ReactionWrapper.propTypes = {
+  context: PropTypes.object.isRequired
+};
+
+export default ReactionWrapper;
--- src/Reaction.js
+++ src/Reaction.js
@@ -6,46 +6,57 @@ import { PersistGate } from 'redux-persist/integration/react';
 import { Provider } from 'react-redux';
 import App from '../../src/App';
 
-export default class Reaction extends Component {
-  static propTypes = {
-    context: PropTypes.object.isRequired
-  };
+function ReactionWrapper(props) {
+  class Reaction extends Component {
+    static propTypes = {
+      context: PropTypes.object.isRequired
+    };
 
-  constructor(props) {
-    super(props);
-    const childContextTypes = {};
-    _.each(props.context, (item, key) => {
-      childContextTypes[key] = PropTypes[typeof item];
-    });
-    this.constructor.childContextTypes = childContextTypes;
-  }
+    getChildContext() {
+      return this.props.context;
+    }
 
-  getChildContext() {
-    return this.props.context;
-  }
+    componentWillMount() {
+      const { context } = this.props;
+      if (context.insertCss) {
+        const rootStyle = require('../../web/styles/root.scss?root=./web/styles/');
+        this.removeCss = context.insertCss(rootStyle);
+      }
+    }
 
-  componentWillMount() {
-    const { context } = this.props;
-    if (context.insertCss) {
-      const rootStyle = require('../../web/styles/root.scss?root=./web/styles/');
-      this.removeCss = context.insertCss(rootStyle);
+    componentWillUnmount() {
+      if (this.removeCss) this.removeCss();
+    }
+
+    render() {
+      const { store, persistor, history } = this.props.context;
+      return (
+        <Provider store={store}>
+          <PersistGate loading={null} persistor={persistor}>
+            <ConnectedRouter history={history}>
+              <App {...this.props} />
+            </ConnectedRouter>
+          </PersistGate>
+        </Provider>
+      );
     }
   }
-
-  componentWillUnmount() {
-    if (this.removeCss) this.removeCss();
-  }
-
-  render() {
-    const { store, persistor, history } = this.props.context;
-    return (
-      <Provider store={store}>
-        <PersistGate loading={null} persistor={persistor}>
-          <ConnectedRouter history={history}>
-            <App {...this.props} />
-          </ConnectedRouter>
-        </PersistGate>
-      </Provider>
-    );
-  }
+  const childContextTypes = {};
+  _.each(props.context, (item, key) => {
+    if (_.isArray(item)) {
+      return (childContextTypes[key] = PropTypes.array.isRequired);
+    }
+    if (_.isFunction(item)) {
+      return (childContextTypes[key] = PropTypes.func.isRequired);
+    }
+    return (childContextTypes[key] = PropTypes[typeof item].isRequired);
+  });
+  Reaction.childContextTypes = childContextTypes;
+  return <Reaction {...props} />;
 }
+
+ReactionWrapper.propTypes = {
+  context: PropTypes.object.isRequired
+};
+
+export default ReactionWrapper;
