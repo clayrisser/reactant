0b42560e9e8c3f6d1b95b2b27a7777ad2f24c458
Jam Risser <jam@codejam.ninja>
2018-08-02 06:47:10 +0530

    Fixed ejs error

--- src/create-webpack-config/getRules.js
+++ src/create-webpack-config/getRules.js
@@ -118,6 +118,7 @@ export default function getRules({ paths, env }) {
         /\.(ts|tsx)$/,
         /\.(vue)$/,
         /\.bmp$/,
+        /\.ejs$/,
         /\.gif$/,
         /\.html?$/,
         /\.jpe?g$/,
