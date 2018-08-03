f1493702d07d943c5a16c0682a65d60e520c2118
Jam Risser <jam@codejam.ninja>
2018-08-03 12:00:34 +0530

    Fixed more expo bugs

--- src/App.js
+++ src/App.js
@@ -1,7 +1,14 @@
 import React, { Component } from 'react';
+import { Font } from 'expo';
 import Routes from '~/routes';
 
 export default class App extends Component {
+  componentDidMount() {
+    Font.loadAsync({
+      Ionicons: require('react-native-vector-icons/Fonts/Ionicons.ttf')
+    });
+  }
+
   render() {
     return <Routes />;
   }
