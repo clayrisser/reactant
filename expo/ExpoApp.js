18ad338a241875a08dbfbab91ec1a7e96536f8f0
Jam Risser <jam@codejam.ninja>
2018-08-07 12:11:10 +0530

    Correctly loaded expo fonts

--- expo/ExpoApp.js
+++ expo/ExpoApp.js
@@ -1,11 +1,19 @@
+import Ionicons from 'react-native-vector-icons/Fonts/Ionicons.ttf';
 import React, { Component } from 'react';
 import Reaction from 'reaction-base/Reaction';
+import { Font } from 'expo';
 import { NativeRouter } from 'react-router-native';
 import { Root, StyleProvider } from 'native-base';
 import getTheme from '../src/theme/components';
 import { material } from '../src/theme/variables';
 
 export default class ExpoApp extends Component {
+  componentDidMount() {
+    Font.loadAsync({
+      Ionicons
+    });
+  }
+
   render() {
     return (
       <NativeRouter {...this.props}>
--- src/App.js
+++ src/App.js
@@ -1,15 +1,7 @@
-import Ionicons from 'react-native-vector-icons/Fonts/Ionicons.ttf';
 import React, { Component } from 'react';
-import { Font } from 'expo';
 import Routes from '~/routes';
 
 export default class App extends Component {
-  componentDidMount() {
-    Font.loadAsync({
-      Ionicons
-    });
-  }
-
   render() {
     return <Routes />;
   }
