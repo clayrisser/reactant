208cb6f65e6e56bccd679a62c0c30233981c5d55
Jam Risser <jam@codejam.ninja>
2018-11-01 02:20:36 -0500

    Added context

--- packages/web-isomorphic/src/createConfig.js
+++ packages/web-isomorphic/src/createConfig.js
@@ -18,14 +18,10 @@ export default function(config) {
       ...config.ignore,
       warnings: [
         ..._.get(config, 'ignore.warnings', []),
-        'bindings/bindings.js 76:22-40',
-        'bindings/bindings.js 76:43-53',
-        'colors.js 138:29-43',
-        'deasync/index.js 31:12-28',
-        'ejs/lib/ejs.js 903:4-22',
-        'ejs/lib/ejs.js 904:2-20',
-        'parse5/lib/index.js 55:23-49',
-        'view.js 81:13-25'
+        '/(express\\/lib\\/view)|(colors\\/lib\\/colors)|(bindings\\/bindings)|' +
+          '(deasync\\/index)|(parse5\\/lib\\/index)|(ejs\\/lib\\/ejs)\\.js.*' +
+          '(Critical dependency: the request of a dependency is an expression)|' +
+          '(require\\.extensions is not supported by webpack\\. Use a loader instead\\.)/'
       ]
     },
     ports: {
