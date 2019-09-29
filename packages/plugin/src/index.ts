e23c51916ed8debe810ccd06a75138359ded74d4
Jam Risser <jam@codejam.ninja>
2019-09-29 18:51:22 +0000

    Added plugin module

--- packages/plugin/src/index.ts
+++ packages/plugin/src/index.ts
@@ -1,1 +1,61 @@
+import path from 'path';
+import { Config } from '@reactant/config';
+import { Plugins, Plugin } from './types';
+
+let _plugins: Plugins;
+
+export function requireDefault<T = any>(moduleName: string): T {
+  const required = require(moduleName);
+  if (required.__esModule && required.default) return required.default;
+  return required;
+}
+
+export async function getReactantPlugins(config: Config): Promise<Plugins> {
+  if (_plugins && Object.keys(_plugins).length) return _plugins;
+  const { paths } = config;
+  const dependencyNames: string[] = Object.keys(
+    require(path.resolve(paths.root, 'package.json')).dependencies
+  );
+  _plugins = dependencyNames
+    .filter((dependencyName: string) => {
+      return !!require(path.resolve(
+        paths.root,
+        'node_modules',
+        dependencyName,
+        'package.json'
+      )).reactantPlugin;
+    })
+    .reduce((platforms: Plugins, platformName: string) => {
+      const platform = {
+        ...requireDefault(
+          path.resolve(
+            paths.root,
+            'node_modules',
+            platformName,
+            require(path.resolve(
+              paths.root,
+              'node_modules',
+              platformName,
+              'package.json'
+            )).reactantPlugin
+          )
+        ),
+        moduleName: platformName
+      };
+      if (!platform.name) platform.name = platformName;
+      else platforms[platformName] = platform;
+      platforms[platform.name] = platform;
+      return platforms;
+    }, {});
+  return _plugins;
+}
+
+export async function getReactantPlugin(
+  platformName: string,
+  config: Config
+): Promise<Plugin> {
+  const platforms: Plugins = await getReactantPlugins(config);
+  return platforms[platformName];
+}
+
 export * from './types';
