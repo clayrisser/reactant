3da34a4023012a1122a93f466aba1b09be6024ec
Jam Risser <jam@codejam.ninja>
2019-10-03 19:17:45 -0500

    Finished test logic

--- packages/web/src/createConfig.ts
+++ packages/web/src/createConfig.ts
@@ -1,9 +1,18 @@
 import path from 'path';
+import util from 'util';
+import { Config, WebpackConfig, Paths, JestConfig } from '@reactant/platform';
 import { RuleSetRule, ResolvePlugin } from 'webpack';
-import { Config, WebpackConfig, Paths } from '@reactant/platform';
 
 interface ModuleScopePlugin {
   appSrcs: string[];
+}
+
+function updatePaths(paths: Paths, webPath: string) {
+  paths.appIndexJs = path.resolve(webPath, 'index.tsx');
+  paths.appSrc = webPath;
+  paths.testsSetup = path.resolve(webPath, 'setupTests.js');
+  paths.appTypeDeclarations = path.resolve(webPath, 'react-app-env.d.ts');
+  paths.proxySetup = path.resolve(webPath, 'setupProxy.js');
 }
 
 function findJSRules(rules: RuleSetRule[]): RuleSetRule[] {
@@ -19,6 +28,49 @@ function findJSRules(rules: RuleSetRule[]): RuleSetRule[] {
 
 export default function createConfig(config: Config): Config {
   if (!config.webpack) config.webpack = {};
+  if (!config.jest) config.jest = {};
+  config.jest.configure = (
+    jestConfig: JestConfig,
+    { paths }: { paths: Paths }
+  ) => {
+    let webPath = path.resolve(config.rootPath, config.platformName);
+    let srcPath = path.resolve(config.rootPath, 'src');
+    if (config.action === 'build') {
+      webPath = path.resolve(
+        config.rootPath,
+        config.paths.build,
+        config.platformName
+      );
+      srcPath = path.resolve(config.rootPath, config.paths.build, 'src');
+    }
+    updatePaths(paths, webPath);
+    jestConfig.roots = [webPath, srcPath];
+    jestConfig.collectCoverageFrom = [
+      ...jestConfig.collectCoverageFrom,
+      `${config.platformName}/**/*.{js,jsx,ts,tsx}`,
+      `!${config.platformName}/**/*.d.ts`
+    ];
+    jestConfig.testMatch = [
+      ...jestConfig.testMatch,
+      '<rootDir>/src/**/tests/**/*.{js,jsx,ts,tsx}',
+      `<rootDir>/${config.platformName}/**/*.{spec,test}.{js,jsx,ts,tsx}`,
+      `<rootDir>/${config.platformName}/**/__tests__/**/*.{js,jsx,ts,tsx}`,
+      `<rootDir>/${config.platformName}/**/tests/**/*.{js,jsx,ts,tsx}`
+    ];
+    if (config.debug) {
+      console.log('paths', paths);
+      console.log(
+        '\n\n======== START JEST ========\n',
+        util.inspect(jestConfig, {
+          colors: true,
+          showHidden: true,
+          depth: null
+        }),
+        '\n========= END JEST =========\n\n'
+      );
+    }
+    return jestConfig;
+  };
   config.webpack.configure = (
     webpackConfig: WebpackConfig,
     { paths }: { paths: Paths }
@@ -33,11 +85,7 @@ export default function createConfig(config: Config): Config {
       );
       srcPath = path.resolve(config.rootPath, config.paths.build, 'src');
     }
-    paths.appIndexJs = path.resolve(webPath, 'index.tsx');
-    paths.appSrc = webPath;
-    paths.testsSetup = path.resolve(webPath, 'setupTests.js');
-    paths.appTypeDeclarations = path.resolve(webPath, 'react-app-env.d.ts');
-    paths.proxySetup = path.resolve(webPath, 'setupProxy.js');
+    updatePaths(paths, webPath);
     webpackConfig.entry = [path.resolve(webPath, 'index.tsx')];
     findJSRules(webpackConfig.module ? webpackConfig.module.rules : []).forEach(
       (rule: RuleSetRule) => {
@@ -61,7 +109,11 @@ export default function createConfig(config: Config): Config {
     if (config.debug) {
       console.log(
         '\n\n======== START WEBPACK ========\n',
-        webpackConfig,
+        util.inspect(webpackConfig, {
+          colors: true,
+          showHidden: true,
+          depth: null
+        }),
         '\n========= END WEBPACK =========\n\n'
       );
     }
