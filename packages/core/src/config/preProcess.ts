ccd3ecef2b981b263fa566026c3a166174282df6
Jam Risser <jam@codejam.ninja>
2019-10-25 08:06:44 +0000

    Updated core

--- packages/core/src/config/preProcess.ts
+++
@@ -1,9 +0,0 @@
-import { Config } from '../types';
-
-export async function preProcess<T = Config>(config: T): Promise<T> {
-  return config;
-}
-
-export function preProcessSync<T = Config>(config: T): T {
-  return config;
-}
--- packages/core/src/index.ts
+++
@@ -1,6 +0,0 @@
-export * from './action';
-export * from './config';
-export * from './logger';
-export * from './platform';
-export * from './plugin';
-export * from './types';
