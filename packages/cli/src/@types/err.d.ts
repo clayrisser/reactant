8324b0af99e12f23fbea3ffeebeb0b160bc3952e
Jam Risser <jam@codejam.ninja>
2019-10-25 02:54:06 -0500

    v0.0.139

--- packages/cli/src/@types/err.d.ts
+++ packages/cli/src/@types/err.d.ts
@@ -1,17 +1,8 @@
-declare class Err extends Error {
-  constructor(message: Err | string, code?: number | string);
-
-  code: number | string;
-
-  isJoi?: boolean;
-
-  originalError?: Err;
-
-  output?: Err;
-
-  statusCode?: number | string;
-}
-
 declare module 'err' {
+  class Err extends Error {
+    constructor(message: Err | string, code?: number | string);
+
+    code: number | string;
+  }
   export = Err;
 }
