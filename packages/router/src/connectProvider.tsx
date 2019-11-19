ef316e16f175f9f76413c25644b5bdfaa9f2b8c8
Jam Risser <jam@codejam.ninja>
2019-11-18 22:07:14 -0600

    Updated router redux provider

--- packages/router/src/connectProvider.tsx
+++ packages/router/src/connectProvider.tsx
@@ -1,4 +1,4 @@
-import React, { FC, ReactNode } from 'react';
+import React, { FC } from 'react';
 import { Provider, Reducer } from '@reactant/redux';
 import {
   ConnectedRouter,
@@ -7,23 +7,26 @@ import {
 } from 'connected-react-router';
 import history from './history';
 
-export interface ProviderProps {
-  children?: ReactNode;
-}
+export interface ProviderProps {}
 
 export default function connectProvider(
   ReduxProvider: Provider
-): FC<ProviderProps> {
-  return (props: ProviderProps) => {
-    return (
-      <ReduxProvider
-        reducers={{
-          router: connectRouter(history) as Reducer
-        }}
-        middlewares={[routerMiddleware(history)]}
-      >
-        <ConnectedRouter history={history}>{props.children}</ConnectedRouter>
-      </ReduxProvider>
-    );
+): (App: React.FC<{}>) => (props: ProviderProps) => JSX.Element {
+  return (App: FC) => {
+    return (props: ProviderProps) => {
+      return (
+        <ReduxProvider
+          reducers={{
+            router: connectRouter(history) as Reducer
+          }}
+          middlewares={[routerMiddleware(history)]}
+        >
+          <ConnectedRouter history={history}>
+            {/* eslint-disable-next-line react/jsx-props-no-spreading */}
+            <App {...props} />
+          </ConnectedRouter>
+        </ReduxProvider>
+      );
+    };
   };
 }
