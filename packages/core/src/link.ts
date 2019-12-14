af47ac5902b910d7ddeedbcb9ba6cd3f46447f18
Jam Risser <jam@codejam.ninja>
2019-12-14 18:06:30 +0000

    Setup reactant-link

---
+++ packages/core/src/link.ts
@@ -0,0 +1,11 @@
+#!/usr/bin/env node
+
+// import fs from 'fs-extra';
+// import pkg from 'npm-pkg-json';
+
+// if (await fs.pathExists(path.resolve(__dirname, '../../../../lerna.json'))) {
+
+// Object.keys(pkg.dependencies).forEach((dependency: string) => {
+//   if (fs.pathExistsSync()) {
+//   }
+// });
