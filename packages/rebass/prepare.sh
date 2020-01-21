c26a4bc95531fca4f44b4f877f529754bc270d2d
Jam Risser <jam@codejam.ninja>
2020-01-21 10:28:05 +0000

    Improved makefiles

--- packages/rebass/prepare.sh
+++ packages/rebass/prepare.sh
@@ -1,5 +1,4 @@
 #!/bin/sh
 
-DIRNAME=$(dirname "$0")
-
-sed -i 's/var require: NodeRequire/var require: NodeJS.Require/' $DIRNAME/node_modules/@types/react-native/index.d.ts
+# DIRNAME=$(dirname "$0")
+# sed -i 's/var require: NodeRequire/var require: NodeJS.Require/' $DIRNAME/node_modules/@types/react-native/index.d.ts
