eb7d6d45f0fca09e2f29e6aae660d26bcd1a831d
Jam Risser <jam@codejam.ninja>
2020-04-08 14:18:48 +0000

    Updated generator

--- packages/generator/src/configuring.js
+++ packages/generator/src/configuring.js
@@ -1,3 +1,29 @@
+import execa from 'execa';
+import fs from 'fs-extra';
+import path from 'path';
+
 export default async function configuring(yo) {
   yo.destinationRoot(yo.answers.destination);
+  if (yo.answers.platform === 'web') {
+    const templatePath = path.resolve(
+      __dirname,
+      '../generators/app/templates/template/web'
+    );
+    const tmpPath = path.resolve(__dirname, '../.tmp');
+    await fs.remove(templatePath);
+    await fs.remove(tmpPath);
+    await fs.mkdirs(tmpPath);
+    await execa(
+      'node',
+      [
+        path.resolve(__dirname, '../node_modules/.bin/create-react-app'),
+        yo.anwers.name,
+        '--typescript',
+      ],
+      {
+        cwd: tmpPath,
+      }
+    );
+    await fs.rename(path.resolve(tmpPath, yo.answers.name), templatePath);
+  }
 }
