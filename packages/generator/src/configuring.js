3416b4e0555d1865029f72823f9ea78dd0d7c870
Jam Risser <jam@codejam.ninja>
2020-04-08 15:26:51 +0000

    Updated generator

--- packages/generator/src/configuring.js
+++ packages/generator/src/configuring.js
@@ -1,27 +1,25 @@
 import execa from 'execa';
 import fs from 'fs-extra';
 import path from 'path';
+import pkgDir from 'pkg-dir';
 
 export default async function configuring(yo) {
   yo.destinationRoot(yo.answers.destination);
-  if (yo.answers.platform === 'web') {
+  if (yo.answers.platform.includes('web')) {
+    const modulePath = pkgDir.sync(require.resolve('create-react-app'));
     const templatePath = path.resolve(
-      __dirname,
-      '../generators/app/templates/template/web'
+      modulePath,
+      'generators/app/templates/template/web'
     );
-    const tmpPath = path.resolve(__dirname, '../.tmp');
+    const tmpPath = path.resolve(modulePath, '.tmp');
     await fs.remove(templatePath);
     await fs.remove(tmpPath);
     await fs.mkdirs(tmpPath);
     await execa(
       'node',
-      [
-        path.resolve(__dirname, '../node_modules/.bin/create-react-app'),
-        yo.anwers.name,
-        '--typescript',
-      ],
+      [path.resolve(modulePath, 'index.js'), yo.answers.name, '--typescript'],
       {
-        cwd: tmpPath,
+        cwd: tmpPath
       }
     );
     await fs.rename(path.resolve(tmpPath, yo.answers.name), templatePath);
