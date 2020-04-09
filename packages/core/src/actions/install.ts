96cbfbcc7348868f487a307f74eaf171819988c1
Jam Risser <jam@codejam.ninja>
2020-04-09 13:42:26 +0000

    Updated install

--- packages/core/src/actions/install.ts
+++ packages/core/src/actions/install.ts
@@ -1,7 +1,7 @@
 import execa from 'execa';
 import fs from 'fs-extra';
 import path from 'path';
-import { ActionResult, Options, PluginAction } from '@reactant/types';
+import { Context, ActionResult, Options, PluginAction } from '@reactant/types';
 import { bootstrap } from '@reactant/context/node';
 import { loadConfig } from '@reactant/config/node';
 import { merge } from '@reactant/context';
@@ -9,6 +9,17 @@ import { where } from '@reactant/helpers';
 import Logger from '../logger';
 import runActions from '.';
 import { preBootstrap, postBootstrap, postProcess } from '../hooks';
+
+export interface Dependancies {
+  [key: string]: string;
+}
+
+export interface Pkg {
+  dependancies?: Dependancies;
+  devDependancies?: Dependancies;
+  name: string;
+  peerDependancies?: Dependancies;
+}
 
 export default async function install(
   platformName?: string,
@@ -25,19 +36,36 @@ export default async function install(
   );
   const logger = new Logger(context.logLevel);
   await runActions(context, logger, pluginActions);
-  let command =
-    (await where(context.config?.preferredPackageManager || '')) || '';
-  if (!command?.length) command = (await where('pnpm')) || '';
-  if (!command?.length) command = (await where('yarn')) || '';
-  if (!command?.length) command = (await where('npm')) || '';
-  if (!command?.length) {
-    throw new Error("please install 'pnpm', 'yarn' or 'npm'");
-  }
   const pkgPath = path.resolve(context.paths.root, 'package.json');
-  const pkgBackupPath = path.resolve(
-    context.paths.root,
-    'package.json.reactant_backup'
-  );
+  if (
+    !(await fs.pathExists(path.resolve(context.paths.root, 'node_modules')))
+  ) {
+    const pkg = await fs.readJson(pkgPath);
+    pkg.dependancies = Object.entries(
+      (pkg?.dependancies || {}) as Dependancies
+    ).reduce(
+      (dependancies: Dependancies, [version, dependancy]: [string, string]) => {
+        if (dependancy.substr(0, 10) === '@reactant/') {
+          dependancies[dependancy] = version;
+        }
+        return dependancies;
+      },
+      {}
+    );
+    pkg.devDependancies = Object.entries(
+      (pkg?.devDependancies || {}) as Dependancies
+    ).reduce(
+      (dependancies: Dependancies, [version, dependancy]: [string, string]) => {
+        if (dependancy.substr(0, 10) === '@reactant/') {
+          dependancies[dependancy] = version;
+        }
+        return dependancies;
+      },
+      {}
+    );
+    pkg.peerDependencies = {};
+    await installDependancies(pkg, context, logger);
+  }
   let pkg = await fs.readJson(pkgPath);
   if (!platformName) {
     await Promise.all(
@@ -74,15 +102,37 @@ export default async function install(
       );
     }
   }
+  await installDependancies(pkg, context, logger);
+  postProcess(context, logger);
+  return null;
+}
+
+export async function installDependancies(
+  pkg: Pkg,
+  context: Context,
+  logger: Logger
+) {
+  let command =
+    (await where(context.config?.preferredPackageManager || '')) || '';
+  if (!command?.length) command = (await where('pnpm')) || '';
+  if (!command?.length) command = (await where('yarn')) || '';
+  if (!command?.length) command = (await where('npm')) || '';
+  if (!command?.length) {
+    throw new Error("please install 'pnpm', 'yarn' or 'npm'");
+  }
+  const pkgPath = path.resolve(context.paths.root, 'package.json');
+  const pkgBackupPath = path.resolve(
+    context.paths.root,
+    'package.json.reactant_backup'
+  );
+
   await fs.rename(pkgPath, pkgBackupPath);
-  await fs.writeJson(pkgPath, pkg);
+  await fs.writeJson(pkgPath, pkg, { spaces: 2 });
   await execa(command, ['install'], {
     stdio: 'inherit',
-    cwd: context.paths.root,
+    cwd: context.paths.root
   });
   if (context.debug) logger.debug(pkg);
   await fs.remove(pkgPath);
   await fs.rename(pkgBackupPath, pkgPath);
-  postProcess(context, logger);
-  return null;
 }
--- packages/generator/src/end.js
+++ packages/generator/src/end.js
@@ -1,3 +1,8 @@
+import execa from 'execa';
+
 export default async function end(yo) {
+  await execa('git', ['init'], {
+    cwd: yo.destinationPath('')
+  });
   return yo;
 }
